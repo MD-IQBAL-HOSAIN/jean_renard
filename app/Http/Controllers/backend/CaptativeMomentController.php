d75a8bd534c1fa9ebabe81fdd49c04f2e4c3ef33
MD-IQBAL-HOSAIN <iqbalmd5692@gmail.com>
2024-09-12 09:37:15 +0600

    update git user

--- app/Http/Controllers/backend/CaptativeMomentController.php
+++ app/Http/Controllers/backend/CaptativeMomentController.php
@@ -14,6 +14,7 @@ class CaptativeMomentController extends Controller
     public function index()
     {
         $captative = Captative::paginate(10);
+        
         return view('backend.captative.index', compact('captative'));
     }
 
